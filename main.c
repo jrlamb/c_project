24bf9cc704d9573d6e87e9ed825ecea071b6af81
lamb <lamb@UTFPRMD-408047.MD.UTFPR>
2022-09-15 08:37:16 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: lamb
+ *
+ * Created on 15 de setembro de 2022, 08:24
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Teste");
+
+    return (EXIT_SUCCESS);
+}
+
